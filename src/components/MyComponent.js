eb3c8dead92659b05489bde056287c823f6237db
Hai Bui <buiduchai277@gmail.com>
2025-10-20 14:04:39 +0700

    video 18

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,10 +7,21 @@ class MyComponent extends React.Component {
         age: 35
     }
 
+    handleonClick(event) {
+        console.log('>>My name Savio');
+
+    }
+
+    handleonMouseOver(event) {
+        console.log(event.pageX);
+    }
+
     render() {
         return (
             <div>
                 my name is {this.state.name} and i am {this.state.age} years old
+                <button onClick={this.handleonClick}>Click me</button>
+                <button onMouseOver={this.handleonMouseOver}>Over me</button>
             </div>
         );
     }
